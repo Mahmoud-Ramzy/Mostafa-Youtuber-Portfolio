198f1b7badce32ad4410c3730368e0b7eef84199
Mahmoud-Ramzy <anatedo4545@gmail.com>
2022-09-26 00:47:17 +0200

    fix animation

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 ScrollReveal({
     reset:false, //repeat move
     distance:'3000px',
-    duration:600,
-    delay:100
+    duration:900,
+    delay:300
 });
 ScrollReveal().reveal('.s1, .main-heading, .about-me', {  origin: 'left' });
-ScrollReveal().reveal('.s2, .m1', { delay: 100, origin: 'bottom' });
-ScrollReveal().reveal('.s3, .vids, .i1', { delay: 100, origin: 'right' });
+ScrollReveal().reveal('.s2, .portfolio-container, .social-links', {origin: 'bottom',duration:'900' });
+ScrollReveal().reveal('.s3,  .i1', { origin: 'right' });
 
 if(window.innerWidth > 906){
     ScrollReveal().reveal('.proto2', { delay: 100, origin: 'bottom' });
